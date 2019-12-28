7ee187a763e2edf6438a964995358b449c8a5a58
Mateusz Fesz <mateuszfesz@wp.pl>
2019-12-28 16:59:25 +0100

    Tweaks

---
+++ src/config.rs
@@ -0,0 +1,107 @@
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+#[derive(Debug)]
+pub struct Config {
+    pub alphabet: Vec<char>,
+    pub drum_settings: Vec<DrumSetting>,
+}
+
+#[derive(Debug)]
+pub struct DrumSetting {
+    pub name: String,
+    pub settings: HashMap<char, char>,
+    pub rot_idx: char,
+}
+
+enum ParserMode {
+    Alphabet,
+    Drum,
+    Rev,
+}
+
+impl Config {
+    pub fn load_from_file<P: AsRef<Path>>(path: P) -> io::Result<Config> {
+        let f = File::open(path)?;
+        let buf = BufReader::new(f);
+
+        let mut config = Config {
+            alphabet: Vec::new(),
+            drum_settings: Vec::new(),
+        };
+
+        let mut mode = ParserMode::Alphabet;
+        let mut real_line_count = 0;
+        for line in buf.lines().filter_map(Result::ok) {
+            if line.starts_with("#") || line.is_empty() {
+                continue;
+            }
+            if line.starts_with("~") {
+                mode = ParserMode::from_str(&line[1..]).unwrap(); //TODO error handling
+            } else {
+                match mode {
+                    ParserMode::Alphabet => {
+                        //TODO split into functions
+                        config.alphabet = line
+                            .trim()
+                            .chars()
+                            .filter(|&c| c != ',')
+                            .collect::<Vec<char>>();
+                    }
+                    ParserMode::Drum => {
+                        let name_len = line.find(",").unwrap(); //TODO: parser error handling
+                        let rot_pos = line.find("&").unwrap();
+                        let set = line[name_len..rot_pos]
+                            .trim()
+                            .chars()
+                            .filter(|&c| c != ',')
+                            .enumerate()
+                            .map(|(idx, el)| (config.alphabet[idx], el))
+                            .collect::<HashMap<char, char>>();
+                        let rot = line[(rot_pos + 1)..].trim().chars().collect::<Vec<char>>()[0];
+                        let drum_setting = DrumSetting {
+                            name: line[0..name_len].to_string(),
+                            settings: set,
+                            rot_idx: rot,
+                        };
+                        config.drum_settings.push(drum_setting);
+                    }
+                    ParserMode::Rev => {
+                        let name_len = line.find(",").unwrap(); //TODO: parser error handling
+                        let set = line[name_len..]
+                            .trim()
+                            .chars()
+                            .filter(|&c| c != ',')
+                            .enumerate()
+                            .map(|(idx, el)| (config.alphabet[idx], el))
+                            .collect::<HashMap<char, char>>();
+                        let drum_setting = DrumSetting {
+                            name: line[0..name_len].to_string(),
+                            settings: set,
+                            rot_idx: ' ',
+                        };
+                        config.drum_settings.push(drum_setting);
+                    }
+                }
+            }
+            real_line_count += 1;
+        }
+        Ok(config)
+    }
+}
+
+impl ParserMode {
+    fn from_str(name: &str) -> Result<ParserMode, String> {
+        //TODO error handling
+        match name {
+            "Alphabet" => Ok(ParserMode::Alphabet),
+            "Drum Settings" => Ok(ParserMode::Drum),
+            "Rev Settings" => Ok(ParserMode::Rev),
+            _ => Err(format!("Unknown mode: {}", name)),
+        }
+    }
+}
