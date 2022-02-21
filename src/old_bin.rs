abc0e1ff408c9e7de2f4b3fcd3144420b4b93d9b
MatiF100 <mateuszfesz@wp.pl>
2022-02-22 00:03:06 +0100

    Refactored file structure

---
+++ src/old_bin.rs
@@ -0,0 +1,56 @@
+use rust_enigma::{old_config::Config, old_lib::Enigma};
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let mut enigma = Enigma::new(Config::load_from_file("config.fesz")?);
+
+    let mut key = String::new();
+    let mut message = String::new();
+
+    let mut left = String::new();
+    let mut mid = String::new();
+    let mut right = String::new();
+    let mut reverse = String::new();
+
+    let [drums, revs] = enigma.aod();
+    for drum in drums {
+        println!("{}: {}", drum.0, drum.1);
+    }
+    print!("Type in number of the right drum: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut right)?;
+
+    print!("Type in number of the middle drum: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut mid)?;
+
+    print!("Type in number of the left drum: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut left)?;
+
+    for rev in revs {
+        println!("{}: {}", rev.0, rev.1);
+    }
+    print!("Type in number of the reversing drum: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut reverse)?;
+
+    let left = left.trim().parse::<usize>().unwrap();
+    let mid = mid.trim().parse::<usize>().unwrap();
+    let right = right.trim().parse::<usize>().unwrap();
+    let reverse = reverse.trim().parse::<usize>().unwrap();
+
+    print!("Key: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut key)?;
+
+    print!("Message: ",);
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut message)?;
+
+    let output = enigma.run(key.trim(), message.trim(), &[right, mid, left, reverse]);
+    println!("Output: {}", output);
+
+    Ok(())
+}
+
---
+++ src/old_lib.rs
@@ -0,0 +1,248 @@
+use old_config::Config;
+
+pub mod old_config;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+mod tests {
+    use super::*;
+    #[test]
+    fn test_process_letter() {
+        let config = config::Config::load_from_file("config.fesz").unwrap();
+
+        let key = process_key("AAB", &config.alphabet);
+        let mut drums: Vec<(Vec<char>, char, i32)> = Vec::new();
+        set_drums(&mut drums, &config.drum_settings, key);
+        assert_eq!(
+            'Z',
+            process_letter(&mut drums, &config.alphabet, &config.alphabet, 'A')
+        );
+    }
+
+    #[test]
+    fn test_message() {
+        let mut enigma = Enigma::new(Config::load_from_file("config.fesz").unwrap());
+        assert_eq!(
+            "RRBLHVGść AZGHVPF",
+            &enigma.run("AAA", "Wiadomość testowa", &[0, 1, 2, 3])
+        );
+    }
+
+    #[test]
+    fn test_subs() {
+        let mut enigma = Enigma::new(Config::load_from_file("config.fesz").unwrap());
+        enigma.substitute('A', 'B');
+        assert_eq!("VXXI", &enigma.run("AAA", "ASDF", &[0, 1, 2, 3]));
+    }
+}
+
+type Drum = (Vec<char>, char, i32);
+
+#[derive(Debug)]
+pub struct Enigma {
+    config: Config,
+    substitutions: Vec<char>,
+    selections: Vec<(Vec<char>, char, i32)>,
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+impl Enigma {
+    pub fn new(config: Config) -> Self {
+        let substitutions = config.alphabet.clone();
+        let selections = Vec::new();
+
+        Enigma {
+            config,
+            substitutions,
+            selections,
+        }
+    }
+
+    pub fn aod<'a>(&'a self) -> [Vec<(usize, &'a str)>; 2] {
+        let drums = &self.config.drum_settings;
+        let mut dr: Vec<(usize, &str)> = Vec::new();
+        let mut rv: Vec<(usize, &str)> = Vec::new();
+
+        for (idx, drum) in drums.iter().enumerate() {
+            if drum.rot_idx != ' ' {
+                dr.push((idx, &drum.name));
+            } else {
+                rv.push((idx, &drum.name));
+            }
+        }
+        [dr, rv]
+    }
+}
+
+impl Enigma {
+    #[cfg(target_arch = "wasm32")]
+    pub fn new() -> Self {
+        let config = Config::load_from_buf(include_str!("../config.fesz").as_bytes()).unwrap();
+        let substitutions = config.alphabet.clone();
+
+        Enigma {
+            config,
+            substitutions,
+            selections: Vec::new(),
+        }
+    }
+
+    pub fn run(&mut self, key: &str, message: &str, idxes: &[usize]) -> String {
+        let key = process_key(key, &self.config.alphabet);
+        let mut drums: Vec<Drum> = Vec::new();
+        let drum_settings = choose_drums(&self.config.drum_settings, idxes);
+        set_drums(&mut drums, &drum_settings, key);
+
+        process_message(
+            &message,
+            &mut drums,
+            &self.config.alphabet,
+            &self.substitutions,
+        )
+    }
+
+    pub fn substitute(&mut self, a: char, b: char) {
+        let a_idx = self.substitutions.iter().position(|&c| c == a).unwrap();
+        let b_idx = self.substitutions.iter().position(|&c| c == b).unwrap();
+        let tmp = self.substitutions[a_idx];
+        self.substitutions[a_idx] = self.substitutions[b_idx];
+        self.substitutions[b_idx] = tmp;
+    }
+}
+/*
+fn encrypt(message: &str, key: &str) -> String {
+    if key.len() != 3 {
+        panic!("key must be of 3 length") //TODO error handling
+    }
+    let config = config::Config::load_from_file("config.fesz").unwrap();
+    let substitutions = config.alphabet.clone();
+    let enigma = Enigma {
+        config,
+        substitutions,
+    };
+    let key = process_key(key, &config.alphabet);
+
+    let mut drums: Vec<(Vec<char>, char, i32)> = Vec::new();
+    set_drums(&mut drums, &config.drum_settings, key);
+
+    process_message(message, &mut drums, &config.alphabet)
+}*/
+
+fn choose_drums(options: &Vec<config::DrumSetting>, selects: &[usize]) -> Vec<config::DrumSetting> {
+    let mut tmp: Vec<config::DrumSetting> = Vec::new();
+    for n in 0..4 {
+        tmp.push(options[selects[n]].clone());
+    }
+    tmp
+}
+
+fn process_key(key: &str, alphabet: &Vec<char>) -> [usize; 3] {
+    let mut result = [0usize; 3];
+    key.chars()
+        .take(3)
+        .map(|c| c.to_ascii_uppercase())
+        .map(|c| alphabet.iter().position(|&x| x == c).unwrap())
+        .enumerate()
+        .for_each(|(idx, el)| result[idx] = el);
+    result
+}
+
+fn process_message(
+    message: &str,
+    drums: &mut Vec<(Vec<char>, char, i32)>,
+    alphabet: &Vec<char>,
+    subs_alphabet: &Vec<char>,
+) -> String {
+    let message = message.to_ascii_uppercase();
+    let encrypted = message
+        .chars()
+        .map(|c| process_letter(drums, alphabet, subs_alphabet, c))
+        .collect::<String>();
+    encrypted
+}
+
+fn process_letter(
+    drums: &mut Vec<(Vec<char>, char, i32)>,
+    alphabet: &Vec<char>,
+    subs_alphabet: &Vec<char>,
+    letter: char,
+) -> char {
+    if !alphabet.contains(&letter) {
+        return letter;
+    }
+    let mut tmp = letter;
+    move_drums(drums, &alphabet);
+
+    let mut idx: i32 = subs_alphabet.iter().position(|&x| x == tmp).unwrap() as i32 - drums[2].2;
+    if idx < 0 {
+        idx = alphabet.len() as i32 + idx
+    }
+    tmp = drums[2].0[idx as usize];
+
+    idx = alphabet.iter().position(|&x| x == tmp).unwrap() as i32 - drums[1].2;
+    if idx < 0 {
+        idx = alphabet.len() as i32 + idx
+    }
+    tmp = drums[1].0[idx as usize];
+
+    idx = alphabet.iter().position(|&x| x == tmp).unwrap() as i32 - drums[0].2;
+    if idx < 0 {
+        idx = alphabet.len() as i32 + idx
+    }
+    tmp = drums[0].0[idx as usize];
+
+    idx = alphabet.iter().position(|&x| x == tmp).unwrap() as i32;
+    tmp = drums[3].0[idx as usize];
+
+    idx = drums[0].0.iter().position(|&x| x == tmp).unwrap() as i32 + drums[0].2;
+    if idx >= alphabet.len() as i32 {
+        idx = idx - alphabet.len() as i32
+    }
+    tmp = alphabet[idx as usize];
+
+    idx = drums[1].0.iter().position(|&x| x == tmp).unwrap() as i32 + drums[1].2;
+    if idx >= alphabet.len() as i32 {
+        idx = idx - alphabet.len() as i32
+    }
+    tmp = alphabet[idx as usize];
+
+    idx = drums[2].0.iter().position(|&x| x == tmp).unwrap() as i32 + drums[2].2;
+    if idx >= alphabet.len() as i32 {
+        idx = idx - alphabet.len() as i32
+    }
+    tmp = subs_alphabet[idx as usize];
+    return tmp;
+}
+
+fn move_drums(drums: &mut Vec<(Vec<char>, char, i32)>, alphabet: &Vec<char>) {
+    drums[2].2 += 1;
+
+    if drums[2].2 == alphabet.iter().position(|&x| x == drums[2].1).unwrap() as i32 {
+        drums[1].2 += 1;
+    }
+    if drums[1].2 == alphabet.iter().position(|&x| x == drums[1].1).unwrap() as i32 {
+        drums[0].2 += 1;
+    }
+    if drums[2].2 >= alphabet.len() as i32 {
+        drums[2].2 = 0;
+    }
+    if drums[2].2 >= alphabet.len() as i32 {
+        drums[2].2 = 0;
+    }
+    if drums[2].2 >= alphabet.len() as i32 {
+        drums[2].2 = 0;
+    }
+}
+
+fn set_drums(
+    drums: &mut Vec<(Vec<char>, char, i32)>,
+    sets: &Vec<config::DrumSetting>,
+    pos_set: [usize; 3],
+) {
+    drums.push((sets[0].settings.clone(), sets[0].rot_idx, pos_set[0] as i32));
+    drums.push((sets[1].settings.clone(), sets[1].rot_idx, pos_set[1] as i32));
+    drums.push((sets[2].settings.clone(), sets[2].rot_idx, pos_set[2] as i32));
+    drums.push((sets[3].settings.clone(), sets[3].rot_idx, -1));
+}
